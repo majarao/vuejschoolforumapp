f4cf60c6729bcf0e4aa8e18a11b576aa9f06a681
majarao <majarao@outlook.com>
2022-05-04 12:45:33 -0300

    git init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <page-home/>
+</template>
+
+<script>
+import PageHome from './components/PageHome.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PageHome
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const forumApp = createApp(App)
+
+forumApp.mount('#app')
