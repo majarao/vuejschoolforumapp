20c989a5fe0f07f33d12243a54f401f9ba2c7733
majarao <majarao@outlook.com>
2022-05-06 13:29:17 -0300

    chapter 2 end

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <page-home/>
+  <div class="container">
+    <page-home/>
+  </div>
 </template>
 
 <script>
@@ -14,12 +16,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/style.css"
 </style>
